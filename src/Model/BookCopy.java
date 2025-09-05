de91e70567e4a41ea79c1674e8e6e857460848e7
Iakalu <151212315+Iakalu@users.noreply.github.com>
2025-09-05 18:35:36 +0700

    cap nhat model

--- src/Model/BookCopy.java
+++ src/Model/BookCopy.java
@@ -10,4 +10,67 @@ public class BookCopy {
     public boolean isAvailable;   // 1 = trong thư viện
     public String status;         // AVAILABLE|CHECKED_OUT|LOST|DAMAGED|RETIRED
     public String shelfLocation;  // vị trí kệ
+	public BookCopy() {
+		super();
+	}
+	public BookCopy(long copyId, long bookId, int copySeq, String callNumber, boolean isAvailable, String status,
+			String shelfLocation) {
+		super();
+		this.copyId = copyId;
+		this.bookId = bookId;
+		this.copySeq = copySeq;
+		this.callNumber = callNumber;
+		this.isAvailable = isAvailable;
+		this.status = status;
+		this.shelfLocation = shelfLocation;
+	}
+	public long getCopyId() {
+		return copyId;
+	}
+	public void setCopyId(long copyId) {
+		this.copyId = copyId;
+	}
+	public long getBookId() {
+		return bookId;
+	}
+	public void setBookId(long bookId) {
+		this.bookId = bookId;
+	}
+	public int getCopySeq() {
+		return copySeq;
+	}
+	public void setCopySeq(int copySeq) {
+		this.copySeq = copySeq;
+	}
+	public String getCallNumber() {
+		return callNumber;
+	}
+	public void setCallNumber(String callNumber) {
+		this.callNumber = callNumber;
+	}
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+	public void setAvailable(boolean isAvailable) {
+		this.isAvailable = isAvailable;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getShelfLocation() {
+		return shelfLocation;
+	}
+	public void setShelfLocation(String shelfLocation) {
+		this.shelfLocation = shelfLocation;
+	}
+	@Override
+	public String toString() {
+		return "BookCopy [copyId=" + copyId + ", bookId=" + bookId + ", copySeq=" + copySeq + ", callNumber="
+				+ callNumber + ", isAvailable=" + isAvailable + ", status=" + status + ", shelfLocation="
+				+ shelfLocation + "]";
+	}
+    
 }
--- src/Model/Books.java
+++ src/Model/Books.java
@@ -1,5 +1,4 @@
-// Models/Books.java
-// POJO cho bảng BOOKS theo ERD v3
+
 package Model;
 
 import java.math.BigDecimal;
@@ -13,5 +12,82 @@ public class Books {
     public int    subjectSeq;    
     public String description;   
     public BigDecimal price;     
-    public String imageURL;  
+    public String imageURL;
+	public Books() {
+		super();
+	}
+	public Books(long bookId, String isbn, String title, String author, int subjectCode, int subjectSeq,
+			String description, BigDecimal price, String imageURL) {
+		super();
+		this.bookId = bookId;
+		this.isbn = isbn;
+		this.title = title;
+		this.author = author;
+		this.subjectCode = subjectCode;
+		this.subjectSeq = subjectSeq;
+		this.description = description;
+		this.price = price;
+		this.imageURL = imageURL;
+	}
+	public long getBookId() {
+		return bookId;
+	}
+	public void setBookId(long bookId) {
+		this.bookId = bookId;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getSubjectCode() {
+		return subjectCode;
+	}
+	public void setSubjectCode(int subjectCode) {
+		this.subjectCode = subjectCode;
+	}
+	public int getSubjectSeq() {
+		return subjectSeq;
+	}
+	public void setSubjectSeq(int subjectSeq) {
+		this.subjectSeq = subjectSeq;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public BigDecimal getPrice() {
+		return price;
+	}
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	public String getImageURL() {
+		return imageURL;
+	}
+	public void setImageURL(String imageURL) {
+		this.imageURL = imageURL;
+	}
+	@Override
+	public String toString() {
+		return "Books [bookId=" + bookId + ", isbn=" + isbn + ", title=" + title + ", author=" + author
+				+ ", subjectCode=" + subjectCode + ", subjectSeq=" + subjectSeq + ", description=" + description
+				+ ", price=" + price + ", imageURL=" + imageURL + "]";
+	}  
+    
 }
